8f9b0464a90f079e4bc6b4217c9964fea6adb6fe
Cozma Alexandru <alexcozma1112@gmail.com>
2024-04-03 16:23:34 +0300

    init commit

---
+++ Client/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module client.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens client.client to javafx.fxml;
+    exports client.client;
+}
